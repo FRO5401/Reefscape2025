9a16baaa272175a69b1fbcd578ef75a5db83150a
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-13 22:24:52 -0400

    More changes and progress being made

--- src/main/java/frc/robot/Subsystems/DrivebaseIOSim.java
+++ src/main/java/frc/robot/Subsystems/DrivebaseIOSim.java
@@ -4,19 +4,26 @@
 
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 /** Add your docs here. */
 public class DrivebaseIOSim implements DrivebaseIO {
     TalonFX leftFalcon = new TalonFX(0);
     TalonFX rightFalcon = new TalonFX(0);
-    
+
+    VoltageOut leftVoltage = new VoltageOut(0);
+    VoltageOut rightVoltage = new VoltageOut(0);
+
     @Override
     public void updateInputs(DrivebaseIOInputs inputs) {
-        var leftSimState =
+
+        var leftSimState = leftFalcon.getSimState();
+        var rightSimState = rightFalcon.getSimState();
+
         // Volts going to drive motors
-        inputs.leftOutputVolts = 0.0;
-        inputs.rightOutputVolts = 0.0;
+        inputs.leftOutputVolts = leftSimState.getMotorVoltage();
+        inputs.rightOutputVolts = rightSimState.getMotorVoltage();
 
         //  Speed of drive train sides
         inputs.leftVelocityMetersPerSecond = 0.0;
@@ -27,13 +34,15 @@ public class DrivebaseIOSim implements DrivebaseIO {
         inputs.rightPositionMeters = 0.0;
 
         //  Amperage and temperature of motors on drive train
-        inputs.leftCurrentAmps = new double[0];
+        inputs.leftCurrentAmps = new double[] {leftSimState.getTorqueCurrent()};
         inputs.leftTempCelsius = new double[0];
-        inputs.rightCurrentAmps = new double[0];
+        inputs.rightCurrentAmps = new double[]{rightSimState.getTorqueCurrent()};
         inputs.rightTempCelsius = new double[0];
     }
 
     @Override
     public void setVolts(double left, double right) {
-        // TODO Auto-generated method stub
+        leftFalcon.setControl(leftVoltage.withOutput(left));
+        rightFalcon.setControl(rightVoltage.withOutput(right));
+
     }}
